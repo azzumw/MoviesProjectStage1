64f36ad5953c04af17956a88b0195964479f2422
awaqar <azzumwaqar@gmail.com>
2018-09-11 15:09:36 +0100

    Loader implemented

--- app/src/main/java/com/example/macintosh/moviesprojectstage1/MainActivity.java
+++ app/src/main/java/com/example/macintosh/moviesprojectstage1/MainActivity.java
@@ -5,11 +5,17 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,9 +31,10 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements MovieAdapter.MovieAdapterOnClickHandler {
+public class MainActivity extends AppCompatActivity implements MovieAdapter.MovieAdapterOnClickHandler, LoaderManager.LoaderCallbacks<ArrayList<Movie>> {
 
-
+    private final static int LOADER_ID = 11;
+    private final String URL_KEY = "URL_KEY";
     private TextView mErrorMessagetv;
 
     private ProgressBar progressBar;
@@ -58,6 +65,8 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
 
         loadMovieData();
 
+
+
     }
 
 
@@ -65,13 +74,25 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
 
         showJsonDataView();
 
-        SharedPreferences pref = PreferenceManager
-                .getDefaultSharedPreferences(getApplicationContext());
+//        SharedPreferences pref = PreferenceManager
+//                .getDefaultSharedPreferences(getApplicationContext());
+//
+//        String END_POINT = pref.getString(getString(R.string.Pref_Key),getString(R.string.popular));
 
-        String END_POINT = pref.getString(getString(R.string.Pref_Key),getString(R.string.popular));
+
+        //new MoviesAsyncTask().execute(END_POINT);
+
+        LoaderManager loaderManager = getSupportLoaderManager();
+        Loader<ArrayList<Movie>> movieLoader = loaderManager.getLoader(LOADER_ID);
+
+        if(movieLoader==null){
+            loaderManager.initLoader(LOADER_ID,null,this);
+        }else{
+            loaderManager.restartLoader(LOADER_ID,null,this);
+        }
 
 
-        new MoviesAsyncTask().execute(END_POINT);
+
     }
 
     /**
@@ -133,59 +154,105 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
 
     }
 
-    public class MoviesAsyncTask extends AsyncTask<String,Void,ArrayList<Movie>>{
+    @NonNull
+    @Override
+    public Loader<ArrayList<Movie>> onCreateLoader(int id, @Nullable final Bundle args) {
+        return new AsyncTaskLoader<ArrayList<Movie>>(this) {
+
+            ArrayList<Movie> movieArrayList;
+
+            @Override
+            protected void onStartLoading() {
+                /* If no arguments were passed, we don't have a query to perform. Simply return. */
+                if (args == null) {
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                /*
+                 * If we already have cached results, just deliver them now. If we don't have any
+                 * cached results, force a load.
+                 */
+                if (movieArrayList != null) {
+                    deliverResult(movieArrayList);
+                } else {
+                    forceLoad();
+                }
+            }
+
+            @Override
+            public void deliverResult(@Nullable ArrayList<Movie> data) {
+                super.deliverResult(data);
+                movieArrayList = data;
+            }
+
+            @Nullable
+            @Override
+            public ArrayList<Movie> loadInBackground() {
+
+                SharedPreferences pref = PreferenceManager
+                        .getDefaultSharedPreferences(getApplicationContext());
+
+                String END_POINT = pref.getString(getString(R.string.Pref_Key),getString(R.string.popular));
 
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progressBar.setVisibility(View.VISIBLE);
+                URL movieRequestURL = NetworkUtils.buildUrl(END_POINT);
+
+                String jsonResult;
+
+                try {
+                    jsonResult = NetworkUtils.getResponseFromHttpUrl(movieRequestURL);
+
+                    ArrayList<Movie> simpleJsonMovieData = NetworkUtils.getJSONData(jsonResult);
+
+
+                    return simpleJsonMovieData;
+
+                } catch (IOException e) {
+                    e.printStackTrace(); return null;
+                } catch (JSONException je){
+                    je.getStackTrace();
+                    return null;
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<ArrayList<Movie>> loader, ArrayList<Movie> data) {
+        progressBar.setVisibility(View.INVISIBLE);
+
+        if(data!= null){
+            showJsonDataView();
+            mMovieAdapter.setMovieData(data);
         }
-
-        @Override
-        protected ArrayList<Movie> doInBackground(String... urls) {
-
-            final String searchURL = urls[0];
-
-            URL movieRequestURL = null;
-
-                movieRequestURL = NetworkUtils.buildUrl(searchURL);
-
-
-            String jsonResult;
-            if(urls.length==0){
-                return null;
-            }
-
-            try {
-                jsonResult = NetworkUtils.getResponseFromHttpUrl(movieRequestURL);
-
-                ArrayList<Movie> simpleJsonMovieData = NetworkUtils.getJSONData(jsonResult);
-
-
-                return simpleJsonMovieData;
-
-            } catch (IOException e) {
-                e.printStackTrace(); return null;
-            } catch (JSONException je){
-                je.getStackTrace();
-                return null;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(ArrayList<Movie> movieData) {
-            progressBar.setVisibility(View.INVISIBLE);
-
-            if(movieData!= null){
-                showJsonDataView();
-                mMovieAdapter.setMovieData(movieData);
-            }
-            else{
-                showErrorMessage();
-            }
+        else{
+            showErrorMessage();
         }
     }
 
+    @Override
+    public void onLoaderReset(@NonNull Loader<ArrayList<Movie>> loader) {
 
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //get the current preference
+        SharedPreferences pref = PreferenceManager
+                .getDefaultSharedPreferences(getApplicationContext());
+
+        String END_POINT = pref.getString(getString(R.string.Pref_Key),getString(R.string.popular));
+        URL url = NetworkUtils.buildUrl(END_POINT);
+        String stringUrl = url.toString();
+
+
+        //put it in the state bundle
+        outState.putString(URL_KEY,stringUrl);
+
+
+    }
 }
