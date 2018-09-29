9f5b7e7f5cfb482c0cc86bbab04eb1e499aa2f5d
awaqar <azzumwaqar@gmail.com>
2018-09-29 15:56:59 +0100

    @Database annotation added to AppDatabase

--- app/src/main/java/com/example/macintosh/moviesprojectstage1/database/AppDatabase.java
+++ app/src/main/java/com/example/macintosh/moviesprojectstage1/database/AppDatabase.java
@@ -1,14 +1,15 @@
 package com.example.macintosh.moviesprojectstage1.database;
 
-import android.arch.persistence.db.SupportSQLiteOpenHelper;
-import android.arch.persistence.room.DatabaseConfiguration;
-import android.arch.persistence.room.InvalidationTracker;
+
+import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.util.Log;
-
+/**
+ * Singleton class
+ * */
+@Database(entities = {Movie.class},version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
   private static final String LOG_TAG = AppDatabase.class.getCanonicalName();
   private static final Object LOCK = new Object();
